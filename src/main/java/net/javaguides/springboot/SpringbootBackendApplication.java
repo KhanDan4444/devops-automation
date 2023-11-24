9cb28082ac37893c139294674387d7e3032b653f
Daniel <lemmadaniel1212@gmail.com>
2023-11-24 16:30:57 +0300

    yeap

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBackendApplication {
-
+//yeap
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
